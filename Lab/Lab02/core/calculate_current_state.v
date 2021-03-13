4fbcb6bca6ba318609e587abf566fa8be1da42de
beta-lux <beta.underalpha@gmail.com>
2021-03-13 17:55:58 +0900

    calculate state draft

--- Lab/Lab02/core/calculate_current_state.v
+++ Lab/Lab02/core/calculate_current_state.v
@@ -1,6 +1,6 @@
 
 `include "vending_machine_def.v"
-	
+`include "state_def.v"	
 
 module calculate_current_state(i_input_coin,i_select_item,item_price,coin_value,current_total,
 input_total, output_total, return_total,current_total_nxt,wait_time,o_return_coin,o_available_item,o_output_item);
@@ -25,7 +25,11 @@ input_total, output_total, return_total,current_total_nxt,wait_time,o_return_coi
 		// TODO: current_total_nxt
 		// You don't have to worry about concurrent activations in each input vector (or array).
 		// Calculate the next current_total state.
-		
+		if(i_select_item) begin
+			current_total_nxt = `STATE_ITEM;
+		end else if (o_output_item) begin
+			current_total_nxt = `STATE_MONEY;
+		end
 	end
 
 	
@@ -34,7 +38,26 @@ input_total, output_total, return_total,current_total_nxt,wait_time,o_return_coi
 	always @(*) begin
 		// TODO: o_available_item
 		// TODO: o_output_item
+		o_output_item = `kNumItems'd0;
+		o_available_item = `kNumItems'd0;
 
+		case(current_total) begin
+			`STATE_MONEY: begin
+				for(i=0; i < `kNumCoins; i = i + 1) begin
+					if(i_input_coin[i] == 1) begin
+						input_total = input_total + coin_value[i];
+					end
+				end
+			end
+
+			`STATE_ITEM: begin
+				for(i = 0; i < `kNumItems; i = i + 1) begin
+					if(i_select_item[i] == 1 && (input_total - return_total) <= item_price) begin
+						o_output_item[i] = 1;
+					end
+				end
+			end
+		endcase
 	end
  
 	
