71ff932b7f85cc53e94e8a6ad128478a1cdfa29e
beta-lux <beta.underalpha@gmail.com>
2021-03-23 00:51:25 +0900

    Register File

--- Lab/Lab03/core/cpu.v
+++ Lab/Lab03/core/cpu.v
@@ -15,7 +15,7 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	input reset_n;									
 	input clk;			
 
-	wire Instruction[31:0];
+	wire Instruction[`WORD_SIZE-1:0];
 
     wire ALUSrc;
     wire [3-1:0] ALUOp;
@@ -27,6 +27,12 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
     wire Jump;
     wire Branch;
 	
+	wire [`WORD_SIZE-1:0] ReadData1;
+	wire [`WORD_SIZE-1:0] ReadData2;
+
+	wire [`WORD_SIZE-1:0] WriteData;
+
 	control_unit MainControl(Instruction, ALUSrc, ALUOp, RegWrite, MemRead, MemtoReg, MemWrite, PCtoReg, Jump, Branch);
+	register_file Registers(ReadData1, ReadData2, Instruction[11:10], Instruction[9:8], Instruction[7:6], WriteData, RegWrite, clk);
 
 endmodule							  																		  
