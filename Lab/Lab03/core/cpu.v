d9602792ca65bb9a7c142c87ead11c529f624ed6
YangSeungWon <35984562+YangSeungWon@users.noreply.github.com>
2021-03-23 21:13:10 +0900

    changed argument format

--- Lab/Lab03/core/cpu.v
+++ Lab/Lab03/core/cpu.v
@@ -37,11 +37,38 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 
 	wire [`NumBits-1:0] WireALUOut;
 
-	control_unit MainControl(Instruction, ALUSrc, ALUOp, RegWrite, MemRead, MemtoReg, MemWrite, PCtoReg, Jump, Branch);
-	register_file Registers(ReadData1, ReadData2, Instruction[11:10], Instruction[9:8], Instruction[7:6], WriteData, RegWrite, clk);
+	control_unit MainControl(.instr(Instruction), 
+							.alu_src(ALUSrc), 
+							.alu_op(ALUOp), 
+							.reg_write(RegWrite), 
+							.mem_read(MemRead), 
+							.mem_to_reg(MemtoReg), 
+							.mem_write(MemWrite), 
+							.PCtoReg(PCtoReg), 
+							.jp(Jump), 
+							.branch(Branch));
 
-	sign_extender SignExtend(Instruction, WireSignExtendOut);
+	register_file Registers(.read_out1(ReadData1), 
+							.read_out2(ReadData2), 
+							.read1(Instruction[11:10]), 
+							.read2(Instruction[9:8]), 
+							.write_reg(Instruction[7:6]), 
+							.write_data(WriteData), 
+							.reg_write(RegWrite), 
+							.clk(clk));
 
-	mux MuxALU(ReadData2, WireSignExtendOut, ALUSrc, WireMuxALUOut);
-	alu ALU(ReadData1, WireMuxALUOut, ALUOp, WireALUOut);
+
+	sign_extender SignExtend(.immediate(Instruction[`IMMD_SIZE-1:0]), 
+							.sign_extended(WireSignExtendOut));
+
+	mux MuxALU(.mux_input_1(ReadData2), 
+				.mux_input_2(WireSignExtendOut), 
+				.selector(ALUSrc), 
+				.mux_output(WireMuxALUOut));
+
+	alu ALU(.alu_input_1(ReadData1), 
+			.alu_input_2(WireMuxALUOut), 
+			.func_code(ALUOp), 
+			.alu_output(WireALUOut));
+
 endmodule							  																		  
