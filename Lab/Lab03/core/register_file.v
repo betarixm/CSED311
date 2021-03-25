a50ea4df067c05102e932f354968e23a413a3522
beta-lux <beta.underalpha@gmail.com>
2021-03-25 21:23:23 +0900

    match size

--- Lab/Lab03/core/register_file.v
+++ Lab/Lab03/core/register_file.v
@@ -6,7 +6,7 @@ module register_file(read_out1, read_out2, read1, read2, write_reg, write_data, 
     output reg [15:0] read_out2;
     input [1:0] read1;
     input [1:0] read2;
-    input [1:0] write_reg;
+    input [`WORD_SIZE - 1:0] write_reg;
     input [15:0] write_data;
     input reg_write;
     input clk;
