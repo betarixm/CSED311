f0fabbd1a823d425a253a015a000d9be4530d84e
beta-lux <beta.underalpha@gmail.com>
2021-03-28 19:17:48 +0900

    impl. io module

--- Lab/Lab03/core/memory_io.v
+++ Lab/Lab03/core/memory_io.v
@@ -11,7 +11,8 @@ module memory_io (
     address_out,
     read_m,
     write_m,
-    data_out
+    data_out,
+    clk
 );
     inout [`WORD_SIZE-1:0] data;
     input sig_fetch;
@@ -21,8 +22,52 @@ module memory_io (
     input input_ready;
     input [`WORD_SIZE-1:0] address_in;
     output [`WORD_SIZE-1:0] address_out;
-    output read_m;
-    output write_m;
-    output [`WORD_SIZE-1:0] data_out;
+    output reg read_m;
+    output reg write_m;
+    output reg [`WORD_SIZE-1:0] data_out;
+
+    input clk;
+
+    reg is_write;
     
+    assign data = (is_write) ? (data_write) : (`WORD_SIZE'bz);
+    assign address_out = address_in;
+
+    always @(posedge clk) begin
+        if(sig_fetch && input_ready) begin
+            is_write = 0;
+            read_m = 1;
+            write_m = 0;
+        end else begin
+            read_m = 0;
+            write_m = 0;
+        end
+    end
+
+    always @(negedge clk) begin
+        if(sig_read && input_ready) begin
+            is_write = 0;
+            read_m = 1;
+            write_m = 0;
+        end else begin
+            read_m = 0;
+            write_m = 0;
+        end
+
+        if(sig_write) begin
+            is_write = 1;
+            read_m = 0;
+            write_m = 1;
+        end else begin 
+            read_m = 0;
+            write_m = 0;
+        end
+    end
+
+    always @(*) begin
+        if(!is_write) begin 
+            data_out = data;
+        end
+    end
+
 endmodule
