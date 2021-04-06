c3a0689933ff9a225477f70e092ec0608a785bc7
YangSeungWon <35984562+YangSeungWon@users.noreply.github.com>
2021-04-07 02:48:30 +0900

    made state definition

---
+++ Lab/Lab04/core/state_def.v
@@ -0,0 +1,18 @@
+`define kStateBits   4
+
+`define STATE_IF_1   0
+`define STATE_IF_2   1
+`define STATE_IF_3   2
+`define STATE_IF_4   3
+
+`define STATE_ID     4
+
+`define STATE_EX_1   5
+`define STATE_EX_2   6
+
+`define STATE_MEM_1  7
+`define STATE_MEM_2  8
+`define STATE_MEM_3  9
+`define STATE_MEM_4 10
+
+`define STATE_WB    11
