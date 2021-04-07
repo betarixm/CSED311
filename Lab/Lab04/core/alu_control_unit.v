8a69336d46d38380acded4b161fc92201ad2d3f6
YangSeungWon <35984562+YangSeungWon@users.noreply.github.com>
2021-04-07 21:22:45 +0900

    lhi, wwd, halt

--- Lab/Lab04/core/alu_control_unit.v
+++ Lab/Lab04/core/alu_control_unit.v
@@ -6,7 +6,7 @@ module alu_control_unit(funct, opcode, ALUOp, clk, funcCode, branchType);
     input [6-1:0] funct;
     input [4-1:0] opcode;
 
-    output reg [3-1:0] funcCode;
+    output reg [4-1:0] funcCode;
     output reg [2-1:0] branchType;
 
     case (opcode)
@@ -26,13 +26,13 @@ module alu_control_unit(funct, opcode, ALUOp, clk, funcCode, branchType);
                 `INST_FUNC_SHR: funcCode = `FUNC_SHR;
                 `INST_FUNC_JPR: 
                 `INST_FUNC_JRL: 
-                `INST_FUNC_WWD: 
-                `INST_FUNC_HLT: 
+                `INST_FUNC_WWD: funcCode = `FUNC_IDN;
+                `INST_FUNC_HLT: funcCode = `FUNC_ZRO;
             endcase
         end
         `ADI_OP: funcCode = `FUNC_ADD;
         `ORI_OP: funcCode = `FUNC_ORR;
-        `LHI_OP: 
+        `LHI_OP: funcCode = `FUNC_LHI;
         `LWD_OP: funcCode = `FUNC_ADD;
         `SWD_OP: funcCode = `FUNC_ADD;
         `BNE_OP: funcCode = `FUNC_SUB;
