717244df1dd3cbeb1030f4c34870fe05bda2c346
YangSeungWon <35984562+YangSeungWon@users.noreply.github.com>
2021-04-07 17:03:15 +0900

    statebit

--- Lab/Lab04/core/state_def.v
+++ Lab/Lab04/core/state_def.v
@@ -1,18 +1,18 @@
 `define kStateBits   4
 
-`define STATE_IF_1   0
-`define STATE_IF_2   1
-`define STATE_IF_3   2
-`define STATE_IF_4   3
+`define STATE_IF_1   `kStateBits'd0
+`define STATE_IF_2   `kStateBits'd1
+`define STATE_IF_3   `kStateBits'd2
+`define STATE_IF_4   `kStateBits'd3
 
-`define STATE_ID     4
+`define STATE_ID     `kStateBits'd4
 
-`define STATE_EX_1   5
-`define STATE_EX_2   6
+`define STATE_EX_1   `kStateBits'd5
+`define STATE_EX_2   `kStateBits'd6
 
-`define STATE_MEM_1  7
-`define STATE_MEM_2  8
-`define STATE_MEM_3  9
-`define STATE_MEM_4 10
+`define STATE_MEM_1  `kStateBits'd7
+`define STATE_MEM_2  `kStateBits'd8
+`define STATE_MEM_3  `kStateBits'd9
+`define STATE_MEM_4 `kStateBits'd10
 
-`define STATE_WB    11
+`define STATE_WB    `kStateBits'd11
