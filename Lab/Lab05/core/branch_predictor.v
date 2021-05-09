50c8c3799e8b1f92fcacd1d8d6f27970e3b857d1
beta-lux <beta.underalpha@gmail.com>
2021-05-10 01:37:21 +0900

    Global-2bit-saturation-BP Sketch

--- Lab/Lab05/core/branch_predictor.v
+++ Lab/Lab05/core/branch_predictor.v
@@ -4,6 +4,11 @@
 `define TRUE   1'b1
 `define FALSE  1'b0
 
+`define STRONG_TAKEN 2'b11
+`define WEAKLY_TAKEN 2'b10
+`define WEAKLY_NOT_TAKEN 2'b01
+`define STRONG_NOT_TAKEN 2'b00
+
 module branch_predictor(clk, reset_n, is_flush, opcode, calculated_pc, current_pc, next_pc, imm);
     input clk;
     input reset_n;
@@ -13,11 +18,33 @@ module branch_predictor(clk, reset_n, is_flush, opcode, calculated_pc, current_p
     input [`WORD_SIZE-1:0] current_pc; // PC from branch resolve stage
     input [`WORD_SIZE-1:0] imm;
 
-    output [`WORD_SIZE-1:0] next_pc;
+    output reg [`WORD_SIZE-1:0] next_pc;
 
-    wire is_branch = (opcode == `BNE_OP) || (opcode == `BEQ_OP) || (opcode == `BGZ_OP) || (opcode == `BLZ_OP);
+    reg[1:0] state;
 
-    // Always Taken
-    assign next_pc = (is_branch) ? (current_pc + 1 + imm) : (current_pc + 1);
+    wire is_branch;
+    wire is_correct;
+
+    initial begin
+        state = `WEAKLY_TAKEN;
+    end
+
+    assign is_branch = (opcode == `BNE_OP) || (opcode == `BEQ_OP) || (opcode == `BGZ_OP) || (opcode == `BLZ_OP);
+    assign is_correct = calculated_pc == next_pc;
+
+    always @(posedge clk) begin
+        case(state)
+            `STRONG_TAKEN:
+                state <= (is_correct) ? (`STRONG_TAKEN) : (`WEAKLY_TAKEN);
+            `WEAKLY_TAKEN:
+                state <= (is_correct) ? (`STRONG_TAKEN) : (`WEAKLY_NOT_TAKEN);
+            `WEAKLY_NOT_TAKEN:
+                state <= (is_correct) ? (`STRONG_NOT_TAKEN) : (`WEAKLY_TAKEN);
+            `STRONG_NOT_TAKEN:
+                state <= (is_correct) ? (`STRONG_NOT_TAKEN) : (`WEAKLY_NOT_TAKEN);
+        endcase
+
+        next_pc <= (state[1]) ? (current_pc + 1 + imm) : (current_pc + 1);
+    end
     
 endmodule
