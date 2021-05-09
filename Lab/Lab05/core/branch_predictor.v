9c23d99c3fe8ae70c21df7130d03bead7f850b04
YangSeungWon <35984562+YangSeungWon@users.noreply.github.com>
2021-05-09 21:38:29 +0900

    fix_errors

--- Lab/Lab05/core/branch_predictor.v
+++ Lab/Lab05/core/branch_predictor.v
@@ -1,13 +1,13 @@
 `include "opcodes.v" 
 `include "env.v"
 
-module branch_predictor(clk, reset_n, is_flush, is_BJ_type, caculated_pc, current_PC, next_PC);
+module branch_predictor(clk, reset_n, is_flush, is_BJ_type, calculated_pc, current_PC, next_PC);
 
     input clk;
     input reset_n;
     input is_flush;
     input is_BJ_type;
-    input [`WORD_SIZE-1:0] caculated_pc;
+    input [`WORD_SIZE-1:0] calculated_pc;
     input [`WORD_SIZE-1:0] current_PC; // PC from branch resolve stage
 
     output [`WORD_SIZE-1:0] next_PC;
