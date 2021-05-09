0f45dcff4704d08dae0128d70cff3cf9ed7b59d6
beta-lux <beta.underalpha@gmail.com>
2021-05-10 02:36:07 +0900

    Jump!

--- Lab/Lab05/core/branch_calculator.v
+++ Lab/Lab05/core/branch_calculator.v
@@ -1,22 +1,29 @@
 `include "opcodes.v"
 `include "env.v"
 
-module branch_calculator (A, B, PC, imm, branch_type, next_pc, bcond);
+module branch_calculator (A, B, PC, imm, branch_type, jump_type, next_pc, bcond);
 
     input [`WORD_SIZE-1:0] A, B, PC;
-    input [`IMMD_SIZE-1:0] imm;
+    input [`WORD_SIZE-1:0] imm;
     input [1:0] branch_type;
+    input [1:0] jump_type;
     output [`WORD_SIZE-1:0] next_pc;
     output reg bcond;
 
-    assign next_pc = (bcond) ? (PC + imm + 1) : (PC + 1);
+    assign next_pc = (bcond) ? (PC + imm + 1) : (
+        (jump_type == `J_JAL || jump_type == `J_JMP) ? ({PC[15:12], imm[11:0]}) : (
+            (jump_type == `J_JPR || jump_type == `J_JRL) ? (A) : (
+                PC + 1
+            )
+        )
+    );
 
     always @(*) begin
         case (branch_type)
             `BRANCH_NE: bcond = (A != B);
             `BRANCH_EQ: bcond = (A == B);
-            `BRANCH_GZ: bcond = ($signed(A) >  0);
-            `BRANCH_LZ: bcond = ($signed(A) <  0);
+            `BRANCH_GZ: bcond = ($signed(A) > 0);
+            `BRANCH_LZ: bcond = ($signed(A) < 0);
         endcase
     end
 
--- Lab/Lab05/core/branch_predictor.v
+++ Lab/Lab05/core/branch_predictor.v
@@ -16,8 +16,16 @@ module branch_predictor(clk, reset_n, is_flush, opcode, calculated_pc, current_p
     output [`WORD_SIZE-1:0] next_pc;
 
     wire is_branch = (opcode == `BNE_OP) || (opcode == `BEQ_OP) || (opcode == `BGZ_OP) || (opcode == `BLZ_OP);
+    wire is_imm_jump = (opcode == `JAL_OP) || (opcode == `JMP_OP);
+    wire is_reg_jump = (opcode == `JRL_OP) || (opcode == `JPR_OP);
 
     // Always Taken
-    assign next_pc = (is_branch) ? (current_pc + 1 + imm) : (current_pc + 1);
+    assign next_pc = (is_branch) ? (current_pc + 1 + imm) : (
+        (is_imm_jump) ? ({current_pc[15:12], imm[11:0]}) : (
+            (is_reg_jump) ? (`WORD_SIZE'b0) : (
+                current_pc + 1
+            )
+        )
+    );
     
 endmodule
