6fceba45b7a60ccb7f94b4747c450cc452d79d4d
beta-lux <beta.underalpha@gmail.com>
2021-05-23 00:22:11 +0900

    Simple Read Logic

--- Lab/Lab06/core/cache.v
+++ Lab/Lab06/core/cache.v
@@ -9,7 +9,7 @@ module cache(c__read_m, c__write_m, addr, i__data, o__data, c__valid, m__read_m,
     input wire c__read_m, c__write_m;
     input wire [`WORD_SIZE-1:0] addr;
     input wire [`WORD_SIZE-1:0] i__data;
-    output wire [`WORD_SIZE-1:0] o__data;
+    output reg [`WORD_SIZE-1:0] o__data;
     output wire c__valid;
     output wire m__read_m, m__write_m;
     output wire [`WORD_SIZE-1:0] m__addr;
@@ -17,7 +17,8 @@ module cache(c__read_m, c__write_m, addr, i__data, o__data, c__valid, m__read_m,
     input wire [`WORD_SIZE-1:0] m__is_stall;
     input wire clk;
     
-    wire is_hit;
+    reg clk_counter;
+    reg is_hit;
     wire idx;
 
     reg [12:0] cache__tag[4];
@@ -26,23 +27,47 @@ module cache(c__read_m, c__write_m, addr, i__data, o__data, c__valid, m__read_m,
     reg [`WORD_SIZE-1:0] cache__lru[4];
     
     assign idx = addr[IDX];
-    assign is_hit = (cache__valid[idx] & (cache__tag[idx] == addr[TAG])) | (cache__valid[idx + 2] && cache__tag[idx + 2] == addr[TAG]);
 
-    always @(*) begin
-        if(is_hit) begin
-            // Hit
-
-        end else begin
-            
-        end
+    initial begin
+        clock_counter = 0;
     end
 
+    // Combinational Logic
+    always @(*) begin
+        
+    end
+
+    // Sequential Logic
     always @(posedge clk) begin
+        // Update Clk Counter
+        // TODO: Reset to 0 when logic end
+        clk_counter <= clk_counter + 1;
+
         // Update LRU
-        cache__lru[0] = cache__lru[0] + 1;
-        cache__lru[1] = cache__lru[1] + 1;
-        cache__lru[2] = cache__lru[2] + 1;
-        cache__lru[3] = cache__lru[3] + 1;
+        // TODO: Do not update the un-hit entry. (On hit logic)
+        cache__lru[0] <= cache__lru[0] + 1;
+        cache__lru[1] <= cache__lru[1] + 1;
+        cache__lru[2] <= cache__lru[2] + 1;
+        cache__lru[3] <= cache__lru[3] + 1;
+
+        // Hit
+        is_hit <= (cache__valid[idx] & (cache__tag[idx] == addr[TAG])) | (cache__valid[idx + 2] && cache__tag[idx + 2] == addr[TAG]);
+
+        if(is_hit) begin
+            // When Hit
+            // Clk counter is estimated to be 1.
+            // Watch out for non-blocking
+
+            if(c__read_m) begin
+                o__data <= (cache__valid[idx]) ? (cache__data[idx][16 * addr[OFF]:16 * addr[OFF]+15]) : (cache__data[idx + 2][16 * addr[OFF]:16 * addr[OFF]+15]);
+                clk_counter <= 0; // Reset clk counter; Need to check race condition
+            end
+        end else begin 
+            // When Not Hit
+            // Clk counter is estimated to be 1.
+            // Watch out for non-blocking
+
+        end
     end
 
 endmodule
