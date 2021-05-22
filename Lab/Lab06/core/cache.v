49399dcaa66d33b581de7d1ac33e1a87a499919f
beta-lux <beta.underalpha@gmail.com>
2021-05-22 23:46:48 +0900

    typo fix

--- Lab/Lab06/core/cache.v
+++ Lab/Lab06/core/cache.v
@@ -1,16 +1,37 @@
 `include "env.v" 
 `include "opcodes.v" 
 
-module cache(c__read_m, c__write_m, addr, i__data, o__data, c__valid, m__read_m, m__write_m, m__addr, m__data_out, m__is_stall);
-    wire c__read_m, c__write_m;
-    wire [`WORD_SIZE-1:0] addr;
-    wire [`WORD_SIZE-1:0] i__data;
-    wire [`WORD_SIZE-1:0] o__data'
-    wire c__valid;
-    wire m__read_m, m__write_m;
-    wire [`WORD_SIZE-1:0] m__addr;
-    wire [`WORD_SIZE-1:0] m__data_out;
-    wire [`WORD_SIZE-1:0] m__is_stall;
+`define TAG 4:15
+`define IDX 3
+`define OFF 0:2
 
+module cache(c__read_m, c__write_m, addr, i__data, o__data, c__valid, m__read_m, m__write_m, m__addr, m__data_out, m__is_stall, clk);
+    input wire c__read_m, c__write_m;
+    input wire [`WORD_SIZE-1:0] addr;
+    input wire [`WORD_SIZE-1:0] i__data;
+    output wire [`WORD_SIZE-1:0] o__data;
+    output wire c__valid;
+    output wire m__read_m, m__write_m;
+    output wire [`WORD_SIZE-1:0] m__addr;
+    output wire [`WORD_SIZE-1:0] m__data_out;
+    input wire [`WORD_SIZE-1:0] m__is_stall;
+    input wire clk;
     
+    wire is_hit;
+
+    reg [12:0] cache__tag[4];
+    reg cache__valid[4];
+    reg [64:0] cache__data[4];
+    reg [`WORD_SIZE-1:0] cache__lru[4];
+    
+    assign is_hit = cache__valid[IDX] | cache__valid[IDX + 2];
+
+    always @(*) begin
+        
+    end
+
+    always @(posedge clk) begin
+        
+    end
+
 endmodule
